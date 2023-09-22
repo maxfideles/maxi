712421f1cb6e0816f50781cbd18131f1307c39bf
maxfideles <maxfideles24@gmail.com>
2023-09-21 21:26:46 -0300

    Page Projects Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MenuBarComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
