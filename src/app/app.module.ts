d138d6709ff6d1b77e4be0f97b89c325927a88a1
maxfideles <maxfideles24@gmail.com>
2024-01-12 21:56:37 -0300

    Hash Location Strategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HashLocationStrategy, LocationStrategy } from '@angular/common'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -32,7 +34,7 @@ import { MainCardComponent } from './pages/home/main-card/main-card.component';
       {path:'projects',component: ProjectsComponent}
     ])
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
